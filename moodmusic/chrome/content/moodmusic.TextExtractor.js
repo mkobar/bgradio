dab6988e8fbf1c207599a534c2b7ed4e88451167
Jack Cheng <zhanpenc@uci.edu>
2014-05-25 01:24:52 -0700

    Added more request options to diffbot to limit bandwidth usage.

--- moodmusic/chrome/content/moodmusic.TextExtractor.js
+++ moodmusic/chrome/content/moodmusic.TextExtractor.js
@@ -2,8 +2,9 @@ var TextExtractor = {
 
 getDocumentText: function(url, successCallback, failureCallback) {
 
-	var apiUrl = "http://api.diffbot.com/v2/article?token=4ba94ea8bdd8300d76961b5888cf46f1" +
-	  "&url=" + escape(url);
+  var token = "4ba94ea8bdd8300d76961b5888cf46f1";
+	var apiUrl = "http://api.diffbot.com/v2/article?token=" + token + "&url=" + escape(url) +
+    "&fields=title,text,tags&paging=false";
 
 	$.get(apiUrl, function(data, textStatus, jqXHR) {
     if (data.error) {
